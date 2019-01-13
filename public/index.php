3e9a785558678371ce720fe497bf027ec596d677
dantemgs <gold_chord@mail.ru>
2019-01-13 03:18:05 +0300

    test

--- public/index.php
+++ public/index.php
@@ -1,7 +1,2 @@
-<?php
-// Создаем новый объект DirectoryIterator
-$dir = new DirectoryIterator("../");
-// Цикл по содержанию директории
-foreach ($dir as $item) {
-    echo $item . "<br>";
-}
+<?
+echo '1';
